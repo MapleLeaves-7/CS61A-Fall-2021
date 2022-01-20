ae649b7a0a45c0df6d697219ef2c509ee20b3c28
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2022-01-20 14:57:17 +0800

    Complete hw04

--- hw/hw04/hw04.py
+++ hw/hw04/hw04.py
@@ -51,13 +51,13 @@ def end(s):
 def planet(size):
     """Construct a planet of some size."""
     assert size > 0
-    "*** YOUR CODE HERE ***"
+    return ['planet', size]
 
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
-    "*** YOUR CODE HERE ***"
+    return w[1]
 
 
 def is_planet(w):
@@ -70,7 +70,7 @@ def examples():
                arm(2, planet(1)))
     u = mobile(arm(5, planet(1)),
                arm(1, mobile(arm(2, planet(3)),
-                              arm(3, planet(2)))))
+                             arm(3, planet(2)))))
     v = mobile(arm(4, t), arm(2, u))
     return (t, u, v)
 
@@ -117,7 +117,18 @@ def balanced(m):
     >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    end_of_left_arm = end(left(m))
+    end_of_right_arm = end(right(m))
+    left_torque = total_weight(end_of_left_arm) * length(left(m))
+    right_torque = total_weight(end_of_right_arm) * length(right(m))
+    if left_torque != right_torque:
+        return False
+    else:
+        for end_of_arm in [end_of_left_arm, end_of_right_arm]:
+            if is_mobile(end_of_arm):
+                if not balanced(end_of_arm):
+                    return False
+        return True
 
 
 def totals_tree(m):
@@ -149,7 +160,10 @@ def totals_tree(m):
     >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return tree(size(m))
+
+    return tree(total_weight(m), [totals_tree(branch) for branch in [end(left(m)), end(right(m))]])
 
 
 def replace_loki_at_leaf(t, lokis_replacement):
@@ -181,7 +195,14 @@ def replace_loki_at_leaf(t, lokis_replacement):
     >>> laerad == yggdrasil # Make sure original tree is unmodified
     True
     """
-    "*** YOUR CODE HERE ***"
+
+    if is_leaf(t):
+        if label(t) == 'loki':
+            return tree(lokis_replacement)
+        else:
+            return tree(label(t))
+
+    return tree(label(t), [replace_loki_at_leaf(b, lokis_replacement) for b in branches(t)])
 
 
 def has_path(t, word):
@@ -215,7 +236,16 @@ def has_path(t, word):
     False
     """
     assert len(word) > 0, 'no path for empty word.'
-    "*** YOUR CODE HERE ***"
+    if label(t) == word:
+        return True
+    else:
+        if label(t) == word[0]:
+            for b in branches(t):
+                if has_path(b, word[1:]):
+                    return True
+            return False
+        else:
+            return False
 
 
 def preorder(t):
@@ -228,7 +258,11 @@ def preorder(t):
     >>> preorder(tree(2, [tree(4, [tree(6)])]))
     [2, 4, 6]
     """
-    "*** YOUR CODE HERE ***"
+    list = [label(t)]
+    if not is_leaf(t):
+        for b in branches(t):
+            list += preorder(b)
+    return list
 
 
 def str_interval(x):
@@ -252,12 +286,12 @@ def interval(a, b):
 
 def lower_bound(x):
     """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[0]
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[1]
 
 
 def str_interval(x):
@@ -276,17 +310,21 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    possible_interval_values = []
+    for x_value in [lower_bound(x), upper_bound(x)]:
+        for y_value in [lower_bound(y), upper_bound(y)]:
+            possible_interval_values.append(x_value * y_value)
+    return interval(min(possible_interval_values), max(possible_interval_values))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
-    "*** YOUR CODE HERE ***"
+    possible_interval_values = []
+    for x_value in [lower_bound(x), upper_bound(x)]:
+        for y_value in [lower_bound(y), upper_bound(y)]:
+            possible_interval_values.append(x_value - y_value)
+    return interval(min(possible_interval_values), max(possible_interval_values))
 
 
 def div_interval(x, y):
@@ -294,6 +332,9 @@ def div_interval(x, y):
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert lower_bound(y) > 0
+    # answer:
+    # assert not (lower_bound(y) <= 0 <= upper_bound(y))
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -318,8 +359,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 10)  # Replace this line!
+    r2 = interval(2, 20)  # Replace this line!
     return r1, r2
 
 
