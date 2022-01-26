ce1d7f2ee15e7059df44c60e937fc98e4668fe4f
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2022-01-26 22:12:14 +0800

    Complete q1 and q2

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,6 +1,17 @@
-(define (my-filter func lst) 'YOUR-CODE-HERE)
+(define (my-filter func lst) 
+  (cond
+    ((null? lst) lst)
+    ((func (car lst)) (cons (car lst) (my-filter func (cdr lst))))
+    (else (my-filter func (cdr lst)))
+  )
+)
 
-(define (interleave s1 s2) 'YOUR-CODE-HERE)
+(define (interleave s1 s2) 
+  (cond
+    ((null? s1) (if (null? s2) nil s2))
+    (else (cons (car s1) (interleave s2 (cdr s1))))
+  )
+)
 
 (define (accumulate merger start n term)
   'YOUR-CODE-HERE)
