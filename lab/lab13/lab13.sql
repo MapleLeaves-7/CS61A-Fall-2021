3aee0f449cad153eaa431b208df5cb4f80c71a9c
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2022-01-31 00:22:12 +0800

    Complete lab13

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,24 +2,35 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet
+  FROM students
+  WHERE color = "blue" AND pet = "dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song
+  FROM students
+  WHERE color = "blue" AND pet = "dog";
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT time, smallest
+  FROM students
+  GROUP BY smallest
+  HAVING count(*) = 1;
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students as a, students as b
+  WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven
+  FROM students as s, numbers as n
+  WHERE s.time = n.time AND s.number = 7 AND n."7" = "True";
 
 
 CREATE TABLE avg_difference AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT round(avg(abs(number - smallest)))
+  FROM students;
 
