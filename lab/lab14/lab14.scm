62b0df88c134d6f9a8f06ffbcaf4dc49cd7d4975
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2022-01-31 16:10:00 +0800

    Complete scheme questions

--- lab/lab14/lab14.scm
+++ lab/lab14/lab14.scm
@@ -1,3 +1,23 @@
-(define (split-at lst n) 'YOUR-CODE-HERE)
+(define (split-at lst n)
+    (cond
+        ((= n 0) (cons nil lst))
+        ((null? lst) (cons lst nil))
+        (else
+            (define prev (split-at (cdr lst) (- n 1)))
+            (cons (cons (car lst) (car prev)) (cdr prev))
+        )
+    )
+)
 
-(define (compose-all funcs) 'YOUR-CODE-HERE)
+(define (compose-all funcs)
+    (define (func-to-return x)
+        (define (apply-all x funcs)
+            (if (null? funcs) 
+                x
+                (apply-all ((car funcs) x) (cdr funcs))
+            )
+        )
+        (apply-all x funcs)
+    )
+    func-to-return
+)
